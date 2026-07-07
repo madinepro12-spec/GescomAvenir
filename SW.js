a5341dc14dea1ec0f846e9b64c197afd8f523186
madinepro12-spec <madine.pro12@gmail.com>
2026-07-07 23:15:12 +0000

    Refactor service worker to use NOM_CACHE

--- SW.js
+++ SW.js
@@ -1,44 +1,47 @@
 
-const CACHE_NAME = 'gescom-avenir-v1';
+const NOM_CACHE = 'gescom-avenir-v1';
 const FICHIERS_A_METTRE_EN_CACHE = [
   './',
   './index.html'
 ];
 
-self.addEventListener('install', function(event){
+self.addEventListener('install', function(event) {
   self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function(cache){
+    caches.open(NOM_CACHE).then(function(cache) {
       return cache.addAll(FICHIERS_A_METTRE_EN_CACHE);
     })
   );
 });
 
-self.addEventListener('activate', function(event){
+self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(function(noms){
+    caches.keys().then(function(noms) {
       return Promise.all(
-        noms.filter(function(nom){ return nom !== CACHE_NAME; })
-            .map(function(nom){ return caches.delete(nom); })
+        noms.filter(function(nom) { return nom !== NOM_CACHE; })
+            .map(function(nom) { return caches.delete(nom); })
       );
     })
   );
   self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event){
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(reponseEnCache){
-      const recuperationReseau = fetch(event.request).then(function(reponseReseau){
-        if(reponseReseau && reponseReseau.status === 200){
-          const copie = reponseReseau.clone();
-          caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, copie); });
-        }
-        return reponseReseau;
-      }).catch(function(){
-        return reponseEnCache;
+    caches.match(event.request).then(function(reponse) {
+      if (reponse) {
+        return reponse;
+      }
+      return fetch(event.request).then(function(reponseReseau) {
+        return caches.open(NOM_CACHE).then(function(cache) {
+          if (event.request.method === 'GET' && reponseReseau.status === 200) {
+            cache.put(event.request, reponseReseau.clone());
+          }
+          return reponseReseau;
+        });
+      }).catch(function() {
+        return caches.match('./index.html');
       });
-      return reponseEnCache || recuperationReseau;
     })
   );
 });
