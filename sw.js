4f08aa99f414804369475ef5d0314daf0c61aec0
madinepro12-spec <madine.pro12@gmail.com>
2026-07-09 23:03:09 +0000

    Change cache version and optimize fetch response
    Updated cache name and improved fetch handling.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const NOM_CACHE = 'gescom-avenir-v1';
+const NOM_CACHE = 'gescom-avenir-v2';
 const FICHIERS_A_METTRE_EN_CACHE = [
   './',
   './index.html'
@@ -28,15 +28,10 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then(function(reponse) {
-      if (reponse) {
-        return reponse;
-      }
-      return fetch(event.request).then(function(reponseReseau) {
+    caches.match(event.request, { ignoreSearch: true }).then(function(reponse) {
+      return reponse || fetch(event.request).then(function(reponseReseau) {
         return caches.open(NOM_CACHE).then(function(cache) {
-          if (event.request.method === 'GET' && reponseReseau.status === 200) {
-            cache.put(event.request, reponseReseau.clone());
-          }
+          cache.put(event.request, reponseReseau.clone());
           return reponseReseau;
         });
       }).catch(function() {
